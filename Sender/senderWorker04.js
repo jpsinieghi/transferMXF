a89f466c85986288709ae9901518d9979cfdcf34
jpsinieghi <jpsinieghi@gmail.com>
2020-06-15 11:34:34 -0300

    worker

--- Sender/senderWorker04.js
+++ Sender/senderWorker04.js
@@ -7,12 +7,12 @@ var countUpdate = 0
 
 function transfer(arquivo){
   
-  var file_media = arquivo.replace('\\','/')
-  var arquivoMXF = file_media.split("/")
-  target = ('http://189.2.14.82:3000/upload/' + arquivoMXF[1]);
+  var file_media = arquivo.replace('\\\\','\\')
+  var arquivoMXF = file_media.split("\\")
+  target = ('http://189.2.14.82:3000/upload/' + arquivoMXF[4]);
   ws = request.post(target)
-  rs = fs.createReadStream(file_media)
-  tamanhoArquivo = fs.statSync(file_media)["size"] * 100
+  rs = fs.createReadStream(arquivo)
+  //tamanhoArquivo = fs.statSync(file_media)["size"] * 100
   ws.on('drain', function () {
       //console.log("Transferindo " + target + "(" + (Math.round(rs.bytesRead / (fs.statSync(file_media))["size"] * 100)) + "%)")
       //console.log("Transferindo " + target + "(" + (Math.round(rs.bytesRead / tamanhoArquivo)) + "%)")
@@ -22,7 +22,7 @@ function transfer(arquivo){
 
                
     rs.on('end', function () {
-      console.log(arquivoMXF[1] + " enviado")
+      console.log(arquivoMXF[4] + " enviado")
       fs.unlinkSync(arquivo)
   
       })
@@ -35,18 +35,19 @@ function transfer(arquivo){
 
 }
 
+
 //'P:\\home\\ftp\\incoming\\Portugal\\MXF\\'
 
-watch('.\\MXF\\', {}, function(evt, name) {
+
+watch('\\\\10.0.95.170\\Portugal\\MXF\\', {}, function(evt, name) {
 
  console.log(evt)
- console.log(name)
-
-  //if(evt == 'update'){
-  //  countUpdate++
-  //  if(countUpdate == 2){
-  //    countUpdate = 0
-  //    transfer(name)
-  //  }
- // }
+ 
+  if(evt == 'update'){
+    countUpdate++
+    if(countUpdate == 2){
+      countUpdate = 0
+      transfer(name)
+    }
+  }
 });
