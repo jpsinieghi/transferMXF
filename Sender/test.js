d5dd887e7beb2617b330c21cab320c6cf05b588e
jpsinieghi <jpsinieghi@gmail.com>
2020-06-15 11:33:52 -0300

    worker04

--- Sender/test.js
+++ Sender/test.js
@@ -1,21 +1,4 @@
 var watch = require('node-watch');
  
-var count = 0
-watch('.\\MXF\\', { recursive: true }, function(evt, name) {
-  
-  if(evt == 'update'){
-    count++
-    console.log(count)
-    if(count == 2){
-      console.log("Console = 2")
-      count = 0
 
-    }
-
-  }
-  
-  console.log('%s changed.', name);
-  console.log(evt);
-    
-});
 
